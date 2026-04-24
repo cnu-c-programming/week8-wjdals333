1b496fbfe6efd7d518ef2dcabe1ff81cd1119d42
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-04-24 09:41:09 +0900

    Update 2-1.c

---
+++ code/2-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    char arr[] = "hello";
+    char *p = "hello";
+
+    arr[0] = 'H';
+    printf("%s\n", arr);
+
+    // p[0] = 'H';
+    // printf("%s\n", p);
+
+
+    return 0;
+}
