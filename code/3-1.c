6182324c95ded1ff9e527012ea4b8718ce982476
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-04-24 10:04:35 +0900

    Update 3-1.c

---
+++ code/3-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char *names[] = {"apple", "mango", "ant", "banana", "cat", "anaconda"};
+    int n = sizeof(names) / sizeof(names[0]);
+
+    for (int i = n-1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (strcmp(names[j], names[j+1]) > 0)
+            {
+                char* temp = names[j];
+                names[j] = names[j+1];
+                names[j+1] = temp;
+
+                
+            }
+            
+        }
+    }
+    
+
+
+
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s\n", names[i]);
+    }
+    
+    return 0;
+}
+
